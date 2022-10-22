d5d9b8643519c7ce517a4cbf3358f8e62e4639b5
roydonGuo <73972404+roydonGuo@users.noreply.github.com>
2022-10-22 02:06:28 +0000

    ✨ NEW FEATURE: Updated By Github Actions

--- js/changebg.js
+++ js/changebg.js
@@ -1,42 +1,66 @@
 if (location.pathname.substring(0, 6) === '/posts') randomChangeBg(true);
 
 var wallhaven = 'https://wallhaven.cc/w/'
-var backimg = [
-    "url(https://w.wallhaven.cc/full/3z/wallhaven-3zppld.jpg)",
-    "url(http://cdn-ali-img-shstaticbz.shanhutech.cn/bizhi/staticwp/202206/96add8f773bcefc346d1c6fe028da943--1508333962.jpg)",
-    "url(http://cdn-ali-img-shstaticbz.shanhutech.cn/bizhi/staticwp/202201/aa6954709fb2b1f2ef25ff73fae3d1e7--4186920932.jpg)",
-    "url(http://cdn-ali-img-shstaticbz.shanhutech.cn/bizhi/staticwp/202201/aa6954709fb2b1f2ef25ff73fae3d1e7--4186920932.jpg)",
-    "url(https://w.wallhaven.cc/full/l3/wallhaven-l3xq62.png)",
-    "url(https://w.wallhaven.cc/full/e7/wallhaven-e7pwdw.jpg)",
-    "url(https://w.wallhaven.cc/full/yj/wallhaven-yjk6ml.jpg)",
-    "url(https://w.wallhaven.cc/full/mp/wallhaven-mprye8.jpg)",
-    "url(https://w.wallhaven.cc/full/q2/wallhaven-q28x3l.jpg)",
-    "url(https://w.wallhaven.cc/full/y8/wallhaven-y8wozk.jpg)",
-    "url(https://w.wallhaven.cc/full/1k/wallhaven-1kzdg1.png)",
-    "url(https://w.wallhaven.cc/full/wy/wallhaven-wyzxvr.jpg)",
-    "url(https://w.wallhaven.cc/full/rd/wallhaven-rdolrq.jpg)",
-    "url(https://w.wallhaven.cc/full/rd/wallhaven-rddgwm.jpg)",
-    "url(https://w.wallhaven.cc/full/ym/wallhaven-ymz61d.jpg)",
-    "url(https://w.wallhaven.cc/full/9m/wallhaven-9mjoy1.png)",
-    "url(https://w.wallhaven.cc/full/z8/wallhaven-z8dp1v.jpg)",
-    "url(https://w.wallhaven.cc/full/v9/wallhaven-v96m6p.jpg)",
-    "url(https://w.wallhaven.cc/full/z8/wallhaven-z8e8qy.jpg)",
-    "url(https://w.wallhaven.cc/full/x8/wallhaven-x8gkpo.jpg)",
-    "url(https://w.wallhaven.cc/full/k7/wallhaven-k7wor1.jpg)",
-    "url(https://w.wallhaven.cc/full/2e/wallhaven-2eroxm.jpg)",
-    "url(https://w.wallhaven.cc/full/e7/wallhaven-e7g71w.jpg)",
-    "url(https://w.wallhaven.cc/full/83/wallhaven-8396gk.jpg)"
+var backimg = []
 
-];
+function getAnime() {
+    // 1. 创建 XHR 对象
+    var xhr = new XMLHttpRequest()
+    // 2. 调用 open 函数
+    xhr.open('GET', 'https://anime-1313439510.cos.ap-shanghai.myqcloud.com/anime/anime.json')
+    // 3. 调用 send 函数，发起 Ajax 请求
+    xhr.send()
+    // 4. 监听 onreadystatechange 事件
+    xhr.onreadystatechange = function () {
+        //监听 xhr 对象的请求状态 readyState ；与服务器响应的状态 status
+        // console.log(xhr.responseText)
+        console.log(xhr);
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            backimg = (xhr.responseText).split(',')
+            console.log('getAnime success' + backimg);
+        }
+    }
+
+}
+
+
+//  [
+//     "url(https://w.wallhaven.cc/full/3z/wallhaven-3zppld.jpg)",
+//     "url(http://cdn-ali-img-shstaticbz.shanhutech.cn/bizhi/staticwp/202206/96add8f773bcefc346d1c6fe028da943--1508333962.jpg)",
+//     "url(http://cdn-ali-img-shstaticbz.shanhutech.cn/bizhi/staticwp/202201/aa6954709fb2b1f2ef25ff73fae3d1e7--4186920932.jpg)",
+//     "url(http://cdn-ali-img-shstaticbz.shanhutech.cn/bizhi/staticwp/202201/aa6954709fb2b1f2ef25ff73fae3d1e7--4186920932.jpg)",
+//     "url(https://w.wallhaven.cc/full/l3/wallhaven-l3xq62.png)",
+//     "url(https://w.wallhaven.cc/full/e7/wallhaven-e7pwdw.jpg)",
+//     "url(https://w.wallhaven.cc/full/yj/wallhaven-yjk6ml.jpg)",
+//     "url(https://w.wallhaven.cc/full/mp/wallhaven-mprye8.jpg)",
+//     "url(https://w.wallhaven.cc/full/q2/wallhaven-q28x3l.jpg)",
+//     "url(https://w.wallhaven.cc/full/y8/wallhaven-y8wozk.jpg)",
+//     "url(https://w.wallhaven.cc/full/1k/wallhaven-1kzdg1.png)",
+//     "url(https://w.wallhaven.cc/full/wy/wallhaven-wyzxvr.jpg)",
+//     "url(https://w.wallhaven.cc/full/rd/wallhaven-rdolrq.jpg)",
+//     "url(https://w.wallhaven.cc/full/rd/wallhaven-rddgwm.jpg)",
+//     "url(https://w.wallhaven.cc/full/ym/wallhaven-ymz61d.jpg)",
+//     "url(https://w.wallhaven.cc/full/9m/wallhaven-9mjoy1.png)",
+//     "url(https://w.wallhaven.cc/full/z8/wallhaven-z8dp1v.jpg)",
+//     "url(https://w.wallhaven.cc/full/v9/wallhaven-v96m6p.jpg)",
+//     "url(https://w.wallhaven.cc/full/z8/wallhaven-z8e8qy.jpg)",
+//     "url(https://w.wallhaven.cc/full/x8/wallhaven-x8gkpo.jpg)",
+//     "url(https://w.wallhaven.cc/full/k7/wallhaven-k7wor1.jpg)",
+//     "url(https://w.wallhaven.cc/full/2e/wallhaven-2eroxm.jpg)",
+//     "url(https://w.wallhaven.cc/full/e7/wallhaven-e7g71w.jpg)",
+//     "url(https://w.wallhaven.cc/full/83/wallhaven-8396gk.jpg)"
+
+// ];
 // 随机背景图片
 function randomChangeBg(flag) {
+    getAnime()
     if (flag) {
         setInterval(function () {
             //获取背景图片总数，生成随机数
             var bgindex = Math.floor(Math.random() * backimg.length)
             //重设背景图片
-            document.querySelector("#web_bg").style.backgroundImage = backimg[bgindex]
-            console.log(document.querySelector("#web_bg").style.background);
+            document.querySelector("#web_bg").style.backgroundImage = 'url(' + backimg[bgindex] + ')'
+            // console.log(document.querySelector("#web_bg").style.background);
         }, 10000)
 
         //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
@@ -58,19 +82,19 @@ var jieliuFlag = true
 var timerFa
 
 function randomBg() {
-
+    getAnime()
     if (jieliuFlag) {
         jieliuFlag = false
         var bgindex = Math.floor(Math.random() * backimg.length)
         //重设背景图片
-        document.querySelector("#web_bg").style.backgroundImage = backimg[bgindex]
-        console.log(document.querySelector("#web_bg").style.background);
+        document.querySelector("#web_bg").style.backgroundImage = 'url(' + backimg[bgindex] + ')'
+        // console.log(document.querySelector("#web_bg").style.background);
         setInterval(function () {
             //获取背景图片总数，生成随机数
             var bgindex = Math.floor(Math.random() * backimg.length)
             //重设背景图片
-            document.querySelector("#web_bg").style.backgroundImage = backimg[bgindex]
-            console.log(document.querySelector("#web_bg").style.background);
+            document.querySelector("#web_bg").style.backgroundImage = 'url(' + backimg[bgindex] + ')'
+            // console.log(document.querySelector("#web_bg").style.background);
         }, 10000)
 
     }
@@ -144,4 +168,3 @@ if (imgbox) {
 /* 调节视频播放速度 */
 // var video= document.getElementById('video-bg');
 // video.playbackRate = 0.5;
-
