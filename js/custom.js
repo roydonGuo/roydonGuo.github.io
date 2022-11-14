fb5b6f7b7b5cc7ce2ddca7b2249fe847b0d02c94
roydonGuo <73972404+roydonGuo@users.noreply.github.com>
2022-11-14 01:00:27 +0000

    ✨ NEW FEATURE: Updated By Github Actions

--- js/custom.js
+++ js/custom.js
@@ -8,11 +8,9 @@ function catalogActive() {
       // 阻止浏览器默认方法
       e.preventDefault()
     }, false)
-
     // 高亮当前页面对应的分类或标签
     let $catalog = document.getElementById(decodeURIComponent(window.location.pathname))
     $catalog.classList.add('selected')
-
     // 滚动当前页面对应的分类或标签到中部
     $list.scrollLeft = ($catalog.offsetLeft - $list.offsetLeft) - ($list.offsetWidth - $catalog.offsetWidth) / 2
   }
