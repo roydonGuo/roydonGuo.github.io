2f42b2ecca36f8aa3e331db1fc353ab559f507e3
roydonGuo <73972404+roydonGuo@users.noreply.github.com>
2023-01-02 08:25:20 +0000

    🎨 IMPROVE: Updated By Github Actions

--- js/changebg.js
+++ js/changebg.js
@@ -1,15 +1,12 @@
 if (location.pathname.substring(0, 6) === '/posts') randomChangeBg(true);
 
-var wallhaven = 'https://wallhaven.cc/w/'
-var backimg = []
-
+// 主页视频背景随机 -----------------------------------------------------------------
 const videoCDN = ["https://gcore.jsdelivr.net/gh/roydonGuo/CDN/videos/hutao.mp4",
     "https://gcore.jsdelivr.net/gh/roydonGuo/CDN/videos/hutao2.mp4",
     "https://gcore.jsdelivr.net/gh/roydonGuo/CDN/videos/nahida.mp4",
     "https://gcore.jsdelivr.net/gh/roydonGuo/CDN/videos/ganyu_heimao.mp4"
 ]
 
-/* 主页视频背景随机 */
 function randomIndexVideo() {
     window.onload = function () {
         var indexVideo = document.querySelector("#index-video");
@@ -20,44 +17,45 @@ function randomIndexVideo() {
     }
 }
 // randomIndexVideo()
+//----------------------------------------------------------------
+let cdnSiteBg = []
 
-function getAnime() {
-    var xhr = new XMLHttpRequest()
-    xhr.open('GET', 'https://anime-1313439510.cos.ap-shanghai.myqcloud.com/anime/anime.json')
-    xhr.send()
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            backimg = (xhr.responseText).split(',')
-            // console.log('getAnime success' + backimg);
+function getCDNSiteBg() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://gcore.jsdelivr.net/gh/roydonGuo/CDN@1.1.7/bgJSON.txt',
+        success: function (res) {
+            cdnSiteBg = res.split(',')
+            // console.log(cdnSiteBg);
         }
-    }
+    })
 }
 
 // 随机背景图片
 function randomChangeBg(flag) {
     if (flag) {
-        getAnime()
+        getCDNSiteBg()
         setInterval(function () {
-            var bgindex = Math.floor(Math.random() * backimg.length)
-            document.querySelector("#web_bg").style.backgroundImage = 'url(' + backimg[bgindex] + ')'
-        }, 20000)
+            var bgindex = Math.floor(Math.random() * cdnSiteBg.length)
+            document.querySelector("#web_bg").style.backgroundImage = 'url(' + cdnSiteBg[bgindex] + ')'
+        }, 30 * 1000)
     }
 }
-var mcBgGif = [
-    "https://bu.dusays.com/2022/11/24/637f50838e27c.gif",
-    "https://bu.dusays.com/2022/11/24/637f50815b2b3.gif",
-    "https://bu.dusays.com/2022/11/24/637f50813ef9c.gif",
-    "https://bu.dusays.com/2022/11/24/637f50869b81d.gif",
-    "https://bu.dusays.com/2022/11/24/637f5086361fa.gif",
-    "https://bu.dusays.com/2022/11/24/637f50860c8c2.gif",
-    "https://bu.dusays.com/2022/11/24/637f5084dc583.gif",
-    "https://bu.dusays.com/2022/11/24/637f507ff3d86.gif",
-    "https://bu.dusays.com/2022/11/24/637f507fd3bc7.gif",
-    "https://bu.dusays.com/2022/11/24/637f507be15b0.gif"
-]
-var mctime
-
+// 首页随机mc背景图片-------------------------------------------------------------------
 function randoMcBgGif() {
+    var mcBgGif = [
+        "https://bu.dusays.com/2022/11/24/637f50838e27c.gif",
+        "https://bu.dusays.com/2022/11/24/637f50815b2b3.gif",
+        "https://bu.dusays.com/2022/11/24/637f50813ef9c.gif",
+        "https://bu.dusays.com/2022/11/24/637f50869b81d.gif",
+        "https://bu.dusays.com/2022/11/24/637f5086361fa.gif",
+        "https://bu.dusays.com/2022/11/24/637f50860c8c2.gif",
+        "https://bu.dusays.com/2022/11/24/637f5084dc583.gif",
+        "https://bu.dusays.com/2022/11/24/637f507ff3d86.gif",
+        "https://bu.dusays.com/2022/11/24/637f507fd3bc7.gif",
+        "https://bu.dusays.com/2022/11/24/637f507be15b0.gif"
+    ]
+    var mctime
     clearInterval(mctime)
     var randombar = document.querySelector("#random");
     if (randombar) {
@@ -68,6 +66,7 @@ function randoMcBgGif() {
     }
 }
 randoMcBgGif()
+//---------------------------------------------------------------------------------------
 // 存数据
 // name：命名 data：数据
 function saveData(name, data) {
@@ -102,18 +101,17 @@ try {
 // // 切换背景函数
 // // 此处的flag是为了每次读取时都重新存储一次,导致过期时间不稳定
 // // 如果flag为0则存储,即设置背景. 为1则不存储,即每次加载自动读取背景.
-
 function changeBg(s, flag) {
     var bg = document.querySelector('#web_bg')
     if (s.charAt(0) === '#') {
         bg.style.backgroundColor = s
         bg.style.backgroundImage = 'none'
-
     } else bg.style.backgroundImage = s
     if (!flag) {
         saveData('blogbg', s)
     }
 }
+
 var nowBgImgUrl = 'url(/img/hutao.jpg)'
 var imgbox = document.querySelector('.fj-gallery')
 if (imgbox) {
@@ -122,12 +120,6 @@ if (imgbox) {
         changeBg(nowBgImgUrl)
     })
 }
-// var toHutao = document.querySelector('.to-hutao')
-// if (toHutao) {
-//     toHutao.addEventListener('click', e => {
-//         if(e) changeBg('url(/img/hutao.jpg)')
-//     })
-// }
 
 // 点击一次，节流
 var jieliuFlag = true
@@ -138,16 +130,16 @@ function randomBg() {
     saveData('randomBgOrNot', randomBgOrNot)
     if (randomBgOrNot && jieliuFlag) {
         jieliuFlag = false
-        getAnime()
-        var bgindex = Math.floor(Math.random() * backimg.length)
+        getCDNSiteBg()
+        var bgindex = Math.floor(Math.random() * cdnSiteBg.length)
         //重设背景图片
-        document.querySelector("#web_bg").style.backgroundImage = 'url(' + backimg[bgindex] + ')'
+        document.querySelector("#web_bg").style.backgroundImage = 'url(' + cdnSiteBg[bgindex] + ')'
         // console.log(document.querySelector("#web_bg").style.background);
         setInterval(function () {
             //获取背景图片总数，生成随机数
-            var bgindex = Math.floor(Math.random() * backimg.length)
+            var bgindex = Math.floor(Math.random() * cdnSiteBg.length)
             //重设背景图片
-            document.querySelector("#web_bg").style.backgroundImage = 'url(' + backimg[bgindex] + ')'
+            document.querySelector("#web_bg").style.backgroundImage = 'url(' + cdnSiteBg[bgindex] + ')'
             // console.log(document.querySelector("#web_bg").style.background);
         }, 20000)
     } else {
@@ -166,61 +158,7 @@ try {
     localStorage.removeItem('randomBgOrNot');
 }
 
-/* 调节视频播放速度 */
-// var video= document.getElementById('video-bg');
-// video.playbackRate = 0.5;
-
 /* winbox */
-// 存数据
-// name：命名 data：数据
-function saveData(name, data) {
-    localStorage.setItem(name, JSON.stringify({
-        'time': Date.now(),
-        'data': data
-    }))
-}
-
-// 取数据
-// name：命名 time：过期时长,单位分钟,如传入30,即加载数据时如果超出30分钟返回0,否则返回数据
-function loadData(name, time) {
-    let d = JSON.parse(localStorage.getItem(name));
-    // 过期或有错误返回 0 否则返回数据
-    if (d) {
-        let t = Date.now() - d.time
-        if (t < (time * 60 * 1000) && t > -1) return d.data;
-    }
-    return 0;
-}
-
-// 上面两个函数如果你有其他需要存取数据的功能，也可以直接使用
-
-// 读取背景
-try {
-    let data = loadData('blogbg', 1440)
-    if (data) changeBg(data, 1)
-    else localStorage.removeItem('blogbg');
-} catch (error) {
-    localStorage.removeItem('blogbg');
-}
-
-// 切换背景函数
-// 此处的flag是为了每次读取时都重新存储一次,导致过期时间不稳定
-// 如果flag为0则存储,即设置背景. 为1则不存储,即每次加载自动读取背景.
-function changeBg(s, flag) {
-    let bg = document.getElementById('web_bg')
-    if (s.charAt(0) == '#') {
-        bg.style.backgroundColor = s
-        bg.style.backgroundImage = 'none'
-    } else bg.style.backgroundImage = s
-    if (!flag) {
-        saveData('blogbg', s)
-    }
-}
-
-// 以下为2.0新增内容
-
-
-// 创建窗口
 var winbox = ''
 
 function createWinbox() {
@@ -251,7 +189,6 @@ function createWinbox() {
     
     <p><button onclick="localStorage.removeItem('blogbg');location.reload();" style="background:pink;display:block;width:100%;padding: 10px 0;border-radius:10px;color:white;"><i class="fa-solid fa-arrows-rotate"></i> 点我恢复默认背景</button></p>
     <p><button onclick="randomBg()" style="background: #ff3456;display:block;width:100%;padding: 10px 0;border-radius:10px;color:white;"><i class="fa-solid iconfont icon-a-tupianyihuifu-14"></i> 开启背景图片随机轮播</button></p>
-    <p class="btn-on" onclick="false"><span class="btn-on-circle"></span><span class="btn-on-text">ON</span></p>
 
     <h2 id="图片（电脑）"><a href="#图片（电脑）" class="headerlink" title="图片（电脑）"></a>图片（电脑）</h2>
     <div class="bgbox">
@@ -259,13 +196,11 @@ function createWinbox() {
     <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://bu.dusays.com/2022/11/17/637617ff5177f.jpg)" class="imgbox" onclick="changeBg('url(https:\//bu.dusays.com/2022/11/17/637617ff5177f.jpg)')"></a>
     <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://bu.dusays.com/2022/11/17/637617ffa49de.png)" class="imgbox" onclick="changeBg('url(https:\//bu.dusays.com/2022/11/17/637617ffa49de.png)')"></a>
     <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://bu.dusays.com/2022/11/18/63765daee75d6.png)" class="imgbox" onclick="changeBg('url(https:\//bu.dusays.com/2022/11/18/63765daee75d6.png)')"></a>
-    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://w.wallhaven.cc/full/28/wallhaven-28rr8m.jpg)" class="imgbox" onclick="changeBg('url(https\://w.wallhaven.cc/full/28/wallhaven-28rr8m.jpg)')"></a>
     </div>
 
     <h2 id="图片（手机）"><a href="#图片（手机）" class="headerlink" title="图片（手机）"></a>图片（手机）</h2>
     <div class="bgbox">
     <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://w.wallhaven.cc/full/7p/wallhaven-7p66we.jpg)" class="pimgbox" onclick="changeBg('url(https\://w.wallhaven.cc/full/7p/wallhaven-7p66we.jpg)')"></a>
-    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://w.wallhaven.cc/full/7p/wallhaven-7p6619.jpg)" class="pimgbox" onclick="changeBg('url(https\://w.wallhaven.cc/full/7p/wallhaven-7p6619.jpg)')"></a>
     <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://w.wallhaven.cc/full/jx/wallhaven-jx88k5.jpg)" class="pimgbox" onclick="changeBg('url(https\://w.wallhaven.cc/full/jx/wallhaven-jx88k5.jpg)')"></a>
     <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://w.wallhaven.cc/full/gp/wallhaven-gpjj97.jpg)" class="pimgbox" onclick="changeBg('url(https\://w.wallhaven.cc/full/gp/wallhaven-gpjj97.jpg)')"></a>
     <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://w.wallhaven.cc/full/57/wallhaven-57x9l9.jpg)" class="pimgbox" onclick="changeBg('url(https\://w.wallhaven.cc/full/57/wallhaven-57x9l9.jpg)')"></a>
@@ -304,45 +239,3 @@ function toggleWinbox() {
     if (document.querySelector('#changeBgBox')) winbox.toggleClass('hide');
     else createWinbox();
 }
-
-// function btn_on_off(type) {
-//     var btn = document.getElementsByClassName("btn-on")[0];
-//     var circle = document.getElementsByClassName("btn-on-circle")[0];
-//     var text = document.getElementsByClassName("btn-on-text")[0];
-//     // let randomBgOrNot = loadData('randomBgOrNot', 1440)
-//     // if (randomBgOrNot) 
-//     if (!type) {
-//         btn.style = "background-color: #ccc;"
-//         circle.style = "left: 36px;background-color: #888;box-shadow: 0 0 10px #888;";
-//         text.style = "right: 26px;color: #888;";
-//         text.innerText = "OFF";
-//         // randomBgOrNot = false
-//         btn.setAttribute("onclick", "btn_on_off(" + type + ")"); // 修改状态
-//     } else {
-//         btn.style = ""
-//         circle.style = "";
-//         text.style = "";
-//         text.innerText = "ON";
-//         // randomBgOrNot = true
-//         randomBg()
-//         btn.setAttribute("onclick", "btn_on_off(" + !type + ")"); // 修改状态
-//     }
-
-// }
-// btn_on_off(false) // 关闭按钮
-// try {
-//     let data = loadData('randomBgOrNot', 1440)
-//     if (data) {
-//         // console.log("data" + data);
-//         randomBg()
-//         // btn_on_off(true)
-//         console.log("btn_on_off(true)");
-//     } else {
-//         // btn_on_off(false)
-//         console.log("btn_on_off(false)");
-//         localStorage.removeItem('randomBgOrNot')
-//     }
-// } catch (error) {
-//     // btn_on_off(false)
-//     localStorage.removeItem('randomBgOrNot');
-// }
