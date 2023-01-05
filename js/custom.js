b278e0578f8ecedb2a1dfc09b51ce932dde0984b
roydonGuo <73972404+roydonGuo@users.noreply.github.com>
2023-01-05 09:51:32 +0000

    💄 UI: Updated By Github Actions

--- js/custom.js
+++ js/custom.js
@@ -201,3 +201,10 @@ function removeRandomBanner() {
 if (location.pathname.substring(0, 5) === '/page') removeRandomBanner()
 
 //重定向浏览器地址
+// pjax.site_handleResponse = pjax.handleResponse;
+// pjax.handleResponse = function (responseText, request, href, options) {
+//   Object.defineProperty(request, 'responseURL', {
+//     value: href
+//   });
+//   pjax.site_handleResponse(responseText, request, href, options);
+// }
