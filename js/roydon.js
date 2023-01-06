08319839102ef1c966d245d0cad11b3114b94d0c
roydonGuo <73972404+roydonGuo@users.noreply.github.com>
2023-01-06 09:56:24 +0000

    💄 UI: Updated By Github Actions

--- js/roydon.js
+++ js/roydon.js
@@ -188,10 +188,10 @@ function toggleWinbox() {
 }
 
 //重定向浏览器地址
-pjax.site_handleResponse = pjax.handleResponse;
-pjax.handleResponse = function(responseText, request, href, options){
-  Object.defineProperty(request,'responseURL',{
-    value: href
-  });
-  pjax.site_handleResponse(responseText,request,href,options);
-}
+// pjax.site_handleResponse = pjax.handleResponse;
+// pjax.handleResponse = function(responseText, request, href, options){
+//   Object.defineProperty(request,'responseURL',{
+//     value: href
+//   });
+//   pjax.site_handleResponse(responseText,request,href,options);
+// }
