8e83120925bc40f84f7a0de0b91dca2b0708e325
roydonGuo <73972404+roydonGuo@users.noreply.github.com>
2023-01-06 09:37:09 +0000

    💄 UI: Updated By Github Actions

--- js/roydon.js
+++ js/roydon.js
@@ -1,5 +1,4 @@
 function whenDOMReady() {
-  // pjax加载完成（切换页面）后需要执行的函数和代码
   roydon.loadbg()
   if (location.pathname == '/essay/') {
     roydon.countUp()
@@ -11,15 +10,12 @@ function whenDOMReady() {
   roydon.danmu()
 }
 
-whenDOMReady() // 打开网站先执行一次
-document.addEventListener("pjax:complete", whenDOMReady) // pjax加载完成（切换页面）后再执行一次
+whenDOMReady()
+document.addEventListener("pjax:complete", whenDOMReady)
 
 roydon.randoMcBgGif()
 
 // roydon.sm("欢迎来到本站","目前存在pjax的bug：)")
-
-// whenDOMReady函数外放一些打开网站之后只需要执行一次的函数和代码，比如一些监听代码。
-// 监听代码只需要执行一次即可，不需要每次加载pjax都执行，会出现一些Bug。至于为什么，我也不知道，可以自己试一下。
 
 var now1 = new Date();
 
