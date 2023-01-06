32c0370145d6fb9753266f5ebea1c15a5696051a
roydonGuo <73972404+roydonGuo@users.noreply.github.com>
2023-01-06 09:29:09 +0000

    🐛 BUG: 💄 UI: Updated By Github Actions

--- js/roydon.js
+++ js/roydon.js
@@ -190,3 +190,12 @@ function toggleWinbox() {
   if (document.querySelector('#changeBgBox')) winbox.toggleClass('hide');
   else createWinbox();
 }
+
+//重定向浏览器地址
+// pjax.site_handleResponse = pjax.handleResponse;
+// pjax.handleResponse = function(responseText, request, href, options){
+//   Object.defineProperty(request,'responseURL',{
+//     value: href
+//   });
+//   pjax.site_handleResponse(responseText,request,href,options);
+// }
