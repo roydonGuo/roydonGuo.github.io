2e80b26c5a067878195cfa9f2a7fa243331d4fd1
roydonGuo <73972404+roydonGuo@users.noreply.github.com>
2023-01-16 16:18:34 +0000

    💄 UI: Updated By Github Actions

--- js/roydon.js
+++ js/roydon.js
@@ -16,6 +16,9 @@ document.addEventListener("pjax:complete", whenDOMReady)
 
 roydon.randoMcBgGif()
 
+// document.onkeydown = function (e) {
+//   if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return btf.snackbarShow("你真坏，不能打开控制台喔!"), event.keyCode = 0, event.returnValue = !1, !1
+// };
 
 // roydon.sm("欢迎来到本站","目前存在pjax的bug：)")
 
