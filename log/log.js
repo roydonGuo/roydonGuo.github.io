f97c78d0ffc3f083f963dd4a0300bb729c328604
roydonGuo <73972404+roydonGuo@users.noreply.github.com>
2023-02-07 11:13:31 +0000

    💄 UI: Updated By Github Actions

--- log/log.js
+++ log/log.js
@@ -1,8 +1,8 @@
 var showtime = function () {
   var nowtime = new Date()
   console.log(nowtime);
-  var tipdate = "距离胡桃复刻还剩："
-  var endtime = new Date("2023/2/7 18:00:00"); //结束时间
+  var tipdate = "胡桃卡池剩余时间："
+  var endtime = new Date("2023/2/28 13:00:00"); //结束时间
   var lefttime = endtime.getTime() - nowtime.getTime(), //距离结束时间的毫秒数
     leftd = Math.floor(lefttime / (1000 * 60 * 60 * 24)), //计算天数
     lefth = Math.floor(lefttime / (1000 * 60 * 60) % 24), //计算小时数
